60eb68ae9eeaa91371812b065fe692ca16205cea
Eclip5e <32602757+Eclip5eLP@users.noreply.github.com>
2020-01-10 10:41:13 +0100

    Add files via upload

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -34,7 +34,7 @@ export class DashboardComponent implements OnInit {
   }
 
   changeState(entry: Entries): void {
-  	this.getListsService.changeState(entry).subscribe();
+  	this.getListsService.changeState(entry).subscribe(this.refresh());
   }
 
   showInfo(entry: Entries): void {
@@ -46,17 +46,33 @@ export class DashboardComponent implements OnInit {
   	this.getListsService.removeEntry(entry).subscribe();
   }
 
+  refresh(): void {
+    window.location.reload();
+  }
+
   getLists(): void {
-  	this.getListsService.getLists()
-  		.subscribe(lists => {
-  			this.tasks = lists.sort((a,b) => {
-  			  return new Date(b.date).getTime() - new Date(a.date).getTime();
-  			});
+    this.getListsService.getLists()
+      .subscribe(lists => {
+        this.tasks = lists.sort((a,b) => {
+          return new Date(b.date).getTime() - new Date(a.date).getTime();
+          return new Date(b.date).getTime() - new Date(a.date).getTime();
+        });
         this.tasks = this.tasks.reverse();
         for (let i = 0; i < this.tasks.length; i++) {
-          this.tasks[i].dispdate = this.datepipe.transform(this.tasks[i].date, 'dd/MM/yyyy');
+          if (this.tasks[i].date === "?") {
+            this.tasks[i].dispdate = "?";
+          } else {
+            this.tasks[i].dispdate = this.datepipe.transform(this.tasks[i].date, 'dd/MM/yyyy');
+          }
         }
-  		}
-  	);
+        let nt = [];
+        for (let i = 0; i < this.tasks.length; i++) {
+          if (this.tasks[i].date !== "?" && this.tasks[i].state !== "done") {
+            nt[nt.length] = this.tasks[i];
+          }
+        }
+        this.tasks = nt;
+      }
+    );
   }
 }
