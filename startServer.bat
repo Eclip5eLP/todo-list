4bb38a60d08aa3e42ff6db91ae03ce4a9988010b
Eclip5e <32602757+Eclip5eLP@users.noreply.github.com>
2020-01-10 10:08:07 +0100

    Fixed startServer.bat

--- startServer.bat
+++ startServer.bat
@@ -6,7 +6,7 @@ if exist "%temp%\nodeServer.bat" del /f /q "%temp%\nodeServer.bat"
 (
 	echo @echo off
 	echo title REST API
-	echo cd %userprofile%\ToDoList\restApi\
+	echo cd %cd%\restApi\
 	echo node server.js
 ) > "%temp%\nodeServer.bat"
 start "REST API" "%temp%\nodeServer.bat"
